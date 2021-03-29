0b90881cb5c26734d10ed4be81f87d16a015d5d8
rarshspeac <52334199+rarshspeac@users.noreply.github.com>
2021-03-29 14:05:32 +0700

    Add files via upload

---
+++ storesda.js
@@ -0,0 +1,6 @@
+var twLSN = [
+'unaizstore.blogspot.com',
+'unaizstore.com',
+];
+
+var redirectUrl = 'http://pesan.link/toko-online';
---
+++ twLSN.js
@@ -0,0 +1,9 @@
+var twLSN = [
+'https://ikrimahherbal.blogspot.com/',
+'unaizstore.blogspot.com',
+'unaizstore.com',
+'herbamaret.com',
+'optimismart.net',
+];
+
+var redirectUrl = 'https://pesan.link/aktivasitokowhatsapp';
